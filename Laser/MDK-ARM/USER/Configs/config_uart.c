86efec4159cc983ce304bc09d2a5aa6772380db4
Will <junweigao1@outlook.com>
2023-07-12 01:49:52 +0800

    激光测距代码可用

--- Laser/MDK-ARM/USER/Configs/config_uart.c
+++ Laser/MDK-ARM/USER/Configs/config_uart.c
@@ -8,8 +8,27 @@
 	
 #include "config_uart.h"
 #include "string.h"
-
+//9101112
 uint8_t data_receive[300];
+uint16_t test;
+uint8_t c1,c2,c3,c4;
+float mea = 0.0f;//ÀåÃ×
+
+
+uint8_t char_2_hex(uint8_t *src)
+{
+    uint8_t desc;
+
+    if((*src >= '0') && (*src <= '9'))
+        desc = *src - 0x30;
+    else if((*src >= 'a') && (*src <= 'f'))
+        desc = *src - 0x57;
+    else if((*src >= 'A') && (*src <= 'F'))
+        desc = *src - 0x37;
+
+    return desc;
+}
+
 
 void USART1_rxDataHandler(uint8_t *rxBuf)
 {
@@ -27,7 +46,17 @@ void USART3_rxDataHandler(uint8_t *rxBuf)
 
 void USART4_rxDataHandler(uint8_t *rxBuf)
 {
-	memcpy(&data_receive, rxBuf, sizeof(rxBuf));
+	memcpy(&data_receive, rxBuf, 20);
+	
+	/*È¡µÍ4Î»*/
+	c1 = char_2_hex(&data_receive[9]) & (0x0F);
+	c2 = char_2_hex(&data_receive[10]) & (0x0F);
+	c3 = char_2_hex(&data_receive[11]) & (0x0F);
+	c4 = char_2_hex(&data_receive[12]) & (0x0F);
+	
+	test = (c1 << 12) | (c2 << 8) | (c3 << 4) | c4;
+	
+	mea = (float)test / 10.0f;
 }
 
 void USART5_rxDataHandler(uint8_t *rxBuf)
